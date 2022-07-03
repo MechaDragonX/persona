d51a32b0b6bd7274833dd94ced79f0701398120b
MechaDragonX <theofficialrv@gmail.com>
2022-07-03 16:39:26 -0700

    Remove unnecessary method and add option params to constructor in Char

--- character.py
+++ character.py
@@ -11,18 +11,21 @@ class Gender(enum.Enum):
 class Character:
     given_name = ''
     surname = ''
-    other_name = ''
+    alias = ''
+    # Species
+    race = ''
     gender = Gender.Male
     age = 0
     profession = ''
     description = ''
 
-    def __init__(self, given_name, surname, gender, age, profession) -> None:
+    def __init__(self, given_name, surname, gender, age, profession, description, race = '', other_name = '') -> None:
         self.given_name = given_name
         self.surname = surname
         self.gender = gender
         self.age = age
         self.profession = profession
-    
-    def import_descroption(self, description) -> None:
         self.description = description
+
+        self.race = race
+        self.alias = other_name
